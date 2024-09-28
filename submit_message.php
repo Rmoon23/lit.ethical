b66ee75d0b868ab20ab5cb188f338da7520b0acc
Rmoon23 <131846621+Rmoon23@users.noreply.github.com>
2024-09-28 22:05:41 +0600

    Create submit_message.php

---
+++ submit_message.php
@@ -0,0 +1,31 @@
+<?php
+// Подключение к базе данных
+$servername = "localhost";
+$username = " lana20fc_ak"; // Ваше имя пользователя MySQL
+$password = "1234Mysql.*"; // Ваш пароль MySQL
+$dbname = " lana20fc_ak"; // Ваше имя базы данных
+
+// Подключаемся к базе данных
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Проверка подключения
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Получаем данные из формы
+$name = $_POST['name'];
+$message = $_POST['message'];
+
+// SQL-запрос для вставки данных в таблицу
+$sql = "INSERT INTO messages (name, message) VALUES ('$name', '$message')";
+
+if ($conn->query($sql) === TRUE) {
+    echo "Message saved successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+// Закрываем соединение
+$conn->close();
+?>
