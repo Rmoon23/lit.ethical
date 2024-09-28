11b9d8ba211833411b53c52b9fa57c29e5216fd4
Rmoon23 <131846621+Rmoon23@users.noreply.github.com>
2024-09-28 22:10:09 +0600

    Update config.php

--- config.php
+++ config.php
@@ -1,17 +1,15 @@
 <?php
-// Параметры подключения к базе данных
-$host = "localhost"; // Обычно "localhost", если сервер базы данных находится на том же сервере
-$dbname = " lana20fc_ak"; // Имя базы данных
-$username = " lana20fc_ak"; // Имя пользователя базы данных
-$password = "1234Mysql.*"; // Пароль пользователя базы данных
+// Настройки подключения к базе данных
+$servername = "localhost";  // Имя сервера базы данных
+$username = " lana20fc_ak";         // Имя пользователя базы данных
+$password = "123Mysql.*";             // Пароль для подключения к базе данных
+$dbname = " lana20fc_ak";       // Имя базы данных
 
-// Попытка подключения
-try {
-    $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
-    // Установка режима ошибки PDO на исключение
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Подключение успешно!";
-} catch (PDOException $e) {
-    echo "Ошибка подключения: " . $e->getMessage();
+// Подключаемся к базе данных
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Проверка соединения
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
 }
 ?>
